08239782de0f1e0b02282bf6162c74e793b051a8
Purit1x <xuy_eason@foxmail.com>
2024-08-20 01:41:09 +0800

    Finish lab10.

--- kernel/proc.h
+++ kernel/proc.h
@@ -20,7 +20,7 @@ struct context {
 
 // Per-CPU state.
 struct cpu {
-  struct proc *proc;          // The process running on this cpu, or null.
+  struct proc* proc;          // The process running on this cpu, or null.
   struct context context;     // swtch() here to enter scheduler().
   int noff;                   // Depth of push_off() nesting.
   int intena;                 // Were interrupts enabled before push_off()?
@@ -82,27 +82,44 @@ struct trapframe {
 
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
+// mmap
+#define VMASIZE 16
+struct vma {
+  int valid;    // 有效位
+  uint64 addr;  // 内存起始地址，可假设始终为0
+  int length;   // 映射字节数
+
+  struct file* f;
+  int prot;     // 内存是否应映射为可读、可写
+  int flags;    // MAP_SHARE 或者 MAP_PRIVATE
+  int fd;       // 文件的描述符
+  int offset;   // 偏移量，可假定为0
+};
+
 // Per-process state
 struct proc {
   struct spinlock lock;
 
   // p->lock must be held when using these:
   enum procstate state;        // Process state
-  void *chan;                  // If non-zero, sleeping on chan
+  void* chan;                  // If non-zero, sleeping on chan
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
 
   // wait_lock must be held when using this:
-  struct proc *parent;         // Parent process
+  struct proc* parent;         // Parent process
 
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
   uint64 sz;                   // Size of process memory (bytes)
   pagetable_t pagetable;       // User page table
-  struct trapframe *trapframe; // data page for trampoline.S
+  struct trapframe* trapframe; // data page for trampoline.S
   struct context context;      // swtch() here to run process
-  struct file *ofile[NOFILE];  // Open files
-  struct inode *cwd;           // Current directory
+  struct file* ofile[NOFILE];  // Open files
+  struct inode* cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  struct vma vma[VMASIZE];   // 进程的vma结构体数组
 };
+
+
